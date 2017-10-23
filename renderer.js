bf31ae1cd87d8f4c6679ce315b5957b7397c007c
youngxblood <crazyeight68@gmail.com>
2017-10-22 21:28:00 -0700

    Added navbar into a Vue component.

--- renderer.js
+++ renderer.js
@@ -1,7 +1,78 @@
 Vue.component('app-nav', {
-    data: function() {
-        return
-    }
+    template: `
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="resources/logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Account
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            View Account
+          </a>
+          <a class="navbar-item">
+            Add Expense
+          </a>
+          <hr class="navbar-divider">
+          <div class="navbar-item">
+            Version 1.0
+          </div>
+        </div>
+      </div>
+    </div>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Dropdown Two
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Account
+          </a>
+          <a class="navbar-item">
+            Elements
+          </a>
+          <a class="navbar-item">
+            Components
+          </a>
+          <hr class="navbar-divider">
+          <div class="navbar-item">
+            Version 1.0
+          </div>
+        </div>
+      </div>
+    </div>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Dropdown Three
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Account
+          </a>
+          <a class="navbar-item">
+            Elements
+          </a>
+          <a class="navbar-item">
+            Components
+          </a>
+          <hr class="navbar-divider">
+          <div class="navbar-item">
+            Version 1.0
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  `
 });
 
 new Vue({
